94709ac0647fb5900a9fc02ba3a2499d71df5687
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-22 19:43:02 -0600

    Se creo la bd y sus modelos

---
+++ peliculas-API/migrations/20260323013810-create-rating.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ratings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      autor: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ratings');
+  }
+};
---
+++ peliculas-API/models/pelicula.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class pelicula extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  pelicula.init({
+    title: DataTypes.STRING,
+    director: DataTypes.STRING,
+    published_year: DataTypes.INTEGER,
+    descripcion: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'pelicula',
+  });
+  return pelicula;
+};
